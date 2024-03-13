e78b3d8b566425047113f86f3e5ac77078a68569
Aleks test <7501130l@gmail.com>
2024-03-13 21:27:12 +0300

    ShopRepository

---
+++ src/test/java/org/example/Exceptions/ShopRepositoryTest.java
@@ -0,0 +1,50 @@
+package org.example.Exceptions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    ShopRepository repo = new ShopRepository();
+    Product product1 = new Product(1, "Shorts", 1500);
+    Product product2 = new Product(2, "Lamp", 3000);
+    Product product3 = new Product(1, "Milk", 100);
+
+
+    @Test
+
+    public void shouldAddProductInRepository() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Product[] expected = {product1, product2, product3};
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldDeleteProductInRepository() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.removeById(1);
+
+        Product[] expected = {product2, product3};
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void test() {
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(80);
+        });
+    }
+}
+
